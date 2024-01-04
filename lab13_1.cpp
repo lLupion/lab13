c1387ba705555846922b0022d8a4eef59c53fe53
Wichuda <Vichudarodna.0569@gmail.com>
2024-01-05 00:00:44 +0700

    done

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,41 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double A[],int N,double B[]){
+    double sum=0,sum_SD=0;
+    int i =0;
+    while(i<N){
+        sum += A[i];
+        i++;
+    }
+    B[0]=sum/N;
+    int a =0;
+    while(a<N){
+        sum_SD+=pow(A[a],2);
+        a++;
+    }
+    B[1]=sqrt((sum_SD/N)-((sum/N)*(sum/N)));
+
+    double pro=1;
+    int b=0;
+    while(b<N){
+        pro *= A[b];
+        b++;
+    }
+    B[2]=pow(pro,(1.0/N));
+    double sum_HM=0;
+    int c=0;
+    while(c<N){
+        sum_HM += 1/A[c];
+        c++;
+    }
+    B[3]= N/sum_HM;
+    double max = A[0], min = A[0];
+    for (int f = 0; f < N; f++) {
+        if (A[f] > max) max = A[f];
+        if (A[f] < min) min = A[f];
+    }
+    B[4] = max;
+    B[5] = min;
+
+}
